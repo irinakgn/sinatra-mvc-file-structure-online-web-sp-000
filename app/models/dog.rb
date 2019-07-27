ea4ef63812cf3bfe04564a8762de08e7967a892b
Irina Kagan <irinagamb@gmail.com>
2019-07-27 21:43:27 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,18 @@
 class Dog
   attr :name, :breed, :age
+  def initialize 
+    @name = name
+    @breed = breed
+    @age = age
+  end 
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
+  def age
+    @age
+  end 
+    
 end
